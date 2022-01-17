5683ce694757b5cc66ccbc8324cc32cd33cec28f
dn110795psv <stanislav.pidhola@privatbank.ua>
2022-01-17 23:14:06 +0200

    Урок 2.Задание 3

---
+++ Lesson2_task3.js
@@ -0,0 +1,14 @@
+/**
+ * Author: Pidgola Stanislav
+ */
+
+
+var val = 'a';
+
+if ( val == 'a' ) {
+    console.log( 'a' );
+} else if ( val == 'b' || val == 'c' || val == 'd' || val == 'e' ) {
+    console.log( 'others ' + val);
+} else {
+    console.log( 'unknown' );
+}
